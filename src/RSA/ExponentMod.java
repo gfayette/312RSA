2351c088006c3a527af2ac35bdb0c7d11fead9d5
G-Fayette <32397749+G-Fayette@users.noreply.github.com>
2019-03-18 18:07:18 -0400

    refactored GenerateKeys

--- src/RSA/ExponentMod.java
+++ src/RSA/ExponentMod.java
@@ -4,52 +4,52 @@ import java.math.BigInteger;
 import java.util.ArrayList;
 
 public class ExponentMod {
-    public long exponentMod(long base, long exponent, long modulus) {
-        ArrayList<Long> powersOfTwo = new ArrayList<>();
-        ArrayList<Long> powersOfTwoModTerms = new ArrayList<>();
+    public BigInteger exponentMod(BigInteger base, BigInteger exponent,
+                            BigInteger modulus) {
+        ArrayList<BigInteger> powersOfTwo = new ArrayList<>();
+        ArrayList<BigInteger> powersOfTwoModTerms = new ArrayList<>();
 
-        long currentExponent = 1;
-        long previousMod = base % modulus;
+        BigInteger currentExponent = BigInteger.ONE;
+        BigInteger previousMod = base.mod(modulus);
 
         powersOfTwo.add(currentExponent);
         powersOfTwoModTerms.add(previousMod);
 
-        while (exponent >= currentExponent * 2) {
-            currentExponent *= 2;
-            previousMod = previousMod * previousMod % modulus;
+        while (currentExponent.multiply(BigInteger.TWO)
+                .compareTo(exponent.add(BigInteger.ONE)) == -1) {
+            currentExponent = currentExponent.multiply(BigInteger.TWO);
+            previousMod =
+                    previousMod.multiply(previousMod).mod(modulus);
 
             powersOfTwo.add(currentExponent);
             powersOfTwoModTerms.add(previousMod);
         }
 
-        long[] binary = new long[powersOfTwo.size()];
-        long e = exponent;
-        long finalTerm = 1;
+        BigInteger e = exponent;
+        BigInteger finalTerm = BigInteger.ONE;
 
-        while (e > 0) {
+        while (e.compareTo(BigInteger.ZERO) == 1) {
             for (int i = powersOfTwo.size() - 1; i >= 0; --i) {
-                if (powersOfTwo.get(i) <= e) {
-                    binary[i] = 1;
-                    finalTerm = finalTerm * powersOfTwoModTerms.get(i) %
-                            modulus;
-                    e -= powersOfTwo.get(i);
-                } else {
-                    binary[i] = 0;
+                if (powersOfTwo.get(i)
+                        .compareTo(e.add(BigInteger.ONE)) == -1) {
+                    finalTerm = finalTerm.multiply(
+                            powersOfTwoModTerms.get(i).mod(modulus));
+                    e = e.subtract(powersOfTwo.get(i));
                 }
             }
         }
 
-        long result = finalTerm % modulus;
-        return result;
+        return finalTerm.mod(modulus);
 
     }
 
     public BigInteger exponentMod2(BigInteger base, BigInteger exponent,
-                             BigInteger modulus) {
+                                   BigInteger modulus) {
 
         BigInteger result = BigInteger.ONE;
         while (exponent.compareTo(BigInteger.ZERO) == 1) {
-            if ((exponent.mod(BigInteger.TWO).compareTo(BigInteger.ONE) == 0)) {
+            if ((exponent.mod(BigInteger.TWO)
+                    .compareTo(BigInteger.ONE) == 0)) {
                 result = (result.multiply(base)).mod(modulus);
             }
             base = (base.multiply(base)).mod(modulus);
@@ -59,13 +59,14 @@ public class ExponentMod {
 
     }
 
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         ExponentMod e = new ExponentMod();
 
-        System.out.println(e.exponentMod(123456, 234567, 345678));
+
         BigInteger b = new BigInteger("123456");
         BigInteger ex = new BigInteger("234567");
         BigInteger m = new BigInteger("345678");
         System.out.println(e.exponentMod2(b, ex, m));
+        System.out.println(e.exponentMod(b, ex, m));
     }
 }
